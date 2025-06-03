54534d26a4375bbac5670f501be735af002eb43b
Puja Kumari <pujakumarijb@gmail.com>
2025-06-03 20:49:48 +0530

    add movie detail page

--- src/controller/movie.controller.js
+++ src/controller/movie.controller.js
@@ -1,3 +1,4 @@
+import lodash from "lodash";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import prisma from "../utils/db.js";
 
@@ -27,4 +28,113 @@ const movies = asyncHandler(async (req, res) => {
   }
 });
 
-export { random, movies };
+const insertFavorite = asyncHandler(async (req, res) => {
+  try {
+    const { email, movieId } = req.body;
+
+    const existingMovies = await prisma.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+
+    if (!existingMovies) throw new Error("Invalid Id");
+
+    const user = await prisma.user.update({
+      where: {
+        email: email || "",
+      },
+      data: {
+        favoriteIds: {
+          push: movieId,
+        },
+      },
+    });
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+});
+
+const deleteFavorite = asyncHandler(async (req, res) => {
+  try {
+    const { email, movieId } = req.query;
+
+    const existingMovies = await prisma.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+
+    if (!existingMovies) throw new Error("Invalid Id");
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    const updateFavoritesIds = lodash.without(user?.favoriteIds, movieId);
+    const updatedUser = await prisma.user.update({
+      where: {
+        email: email || "",
+      },
+      data: {
+        favoriteIds: updateFavoritesIds,
+      },
+    });
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+});
+
+const getFavorite = asyncHandler(async (req, res) => {
+  try {
+    const { email } = req.params;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    const favoriteMovies = await prisma.movie.findMany({
+      where: {
+        id: {
+          in: user?.favoriteIds,
+        },
+      },
+    });
+    return res.status(200).json(favoriteMovies);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+});
+const getMovieDetails = asyncHandler(async (req, res) => {
+  try {
+    const { movieId } = req.params;
+    if (typeof movieId !== "string") {
+      throw new Error("Invaild ID");
+    }
+    if (!movieId) {
+      throw new Error("Invaild ID");
+    }
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+    if (!movie) {
+      throw new Error("Invaild ID");
+    }
+    return res.status(200).json(movie);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+});
+export { random, movies, insertFavorite, deleteFavorite, getFavorite, getMovieDetails};
--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
-import { movies, random } from "../controller/movie.controller.js";
+import {
+  deleteFavorite,
+  getFavorite,
+  getMovieDetails,
+  insertFavorite,
+  movies,
+  random,
+} from "../controller/movie.controller.js";
 
 const router = Router();
 
 router.route("/random").get(random);
 router.route("/movies").get(movies);
+router.route("/favorite").post(insertFavorite);
+router.route("/favorite").delete(deleteFavorite);
+router.route("/favorite/:email").get(getFavorite);
+
+router.route("/:movieId").get(getMovieDetails);
 
 export default router;
